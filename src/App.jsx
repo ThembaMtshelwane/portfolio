92a9367d07ecae3c9bf46f533e50f8ff933777d9
ThembaMtshelwane <thembamm3@gmail.com>
2024-07-22 14:17:18 +0200

    General styles: Background opacity fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import HeroSection from "./Components/HeroSection/HeroSection";
 import AboutSection from "./Components/AboutSection/AboutSection.tsx";
 import SkillsSection from "./Components/SkillsSection/SkillsSection.tsx";
-
+import ProjectsSection from "./Components/ProjectsSection/ProjectsSection.tsx";
+import "./App.css";
 const App = () => {
   return (
-    <>
+    <section className="main-container">
+      <section className="background-colour"></section>
       <HeroSection />
       <AboutSection />
       <SkillsSection />
-    </>
+      <ProjectsSection />
+    </section>
   );
 };
 
