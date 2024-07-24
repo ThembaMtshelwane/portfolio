b160cb7f8bd087838c4d99c30c478a12c649ac7b
ThembaMtshelwane <thembamm3@gmail.com>
2024-07-24 10:12:05 +0200

    Skills Section: Added work experience

--- src/Components/SkillsSection/SkillsSection.tsx
+++ src/Components/SkillsSection/SkillsSection.tsx
@@ -12,11 +12,28 @@ const SkillsSection = () => {
     );
   });
   return (
-    <section className="skills-section section-container">
-      <h2>Skills</h2>
-      <section className="skills-frontend">
-        <h4>Frontend Development</h4>
-        <ul>{skillsListItems}</ul>
+    <section className="skills-section">
+      <h2>Experience</h2>
+      <section className="experience-container">
+        <section className="work-experience-container">
+          <section className="work-experiece">
+            <p className="position">Full-stack Web Developer</p>
+            <p className="duration">1 year</p>
+            <p className="company">Wits Vaction work for Dr Philippe Ferrer</p>
+          </section>
+          <section className="work-experiece">
+            <p className="position">Teaching Assistant</p>
+            <p className="duration">3 months</p>
+            <p className="company">Wits University</p>
+          </section>
+        </section>
+        <section className="cv-container">
+          <button>CV FOR DOWNLOAD</button>
+        </section>
+        <section className="skills-frontend">
+          {/* <h4>Technologies</h4> */}
+          <ul>{skillsListItems}</ul>
+        </section>
       </section>
     </section>
   );
