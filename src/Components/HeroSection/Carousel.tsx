4bc3cf4409c0e88295cac58e82d247cf95a86c63
Themba Mtshelwane <Thembamm3@gmail.com>
2024-08-31 12:17:54 +0200

    Add new projects and about summary updated

--- src/Components/HeroSection/Carousel.tsx
+++ src/Components/HeroSection/Carousel.tsx
@@ -1,44 +1,18 @@
-import React, { useEffect, useState } from "react";
-import "./carousel.css";
+import React from 'react'
+import './carousel.css'
 
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
-import { FaHtml5 } from "react-icons/fa";
-import { IoLogoCss3 } from "react-icons/io5";
-import { IoLogoJavascript } from "react-icons/io5";
-import { FaReact } from "react-icons/fa";
-import { RiNextjsFill } from "react-icons/ri";
-import { IoLogoFirebase } from "react-icons/io5";
-import { SiChakraui } from "react-icons/si";
-import { SiTypescript } from "react-icons/si";
+import { RiTailwindCssFill } from 'react-icons/ri'
+import { FaHtml5 } from 'react-icons/fa'
+import { IoLogoCss3 } from 'react-icons/io5'
+import { IoLogoJavascript } from 'react-icons/io5'
+import { FaReact } from 'react-icons/fa'
+import { RiNextjsFill } from 'react-icons/ri'
+import { IoLogoFirebase } from 'react-icons/io5'
+import { SiChakraui } from 'react-icons/si'
+import { SiTypescript } from 'react-icons/si'
 
 const Carousel = () => {
   return (
-    // <div className="logos">
-    //   <div className="logos-slide">
-    //     <img src="./logos/3m.svg" />
-    //     <img src="./logos/barstool-store.svg" />
-    //     <img src="./logos/budweiser.svg" />
-    //     <img src="./logos/buzzfeed.svg" />
-    //     <img src="./logos/forbes.svg" />
-    //     <img src="./logos/macys.svg" />
-    //     <img src="./logos/menshealth.svg" />
-    //     <img src="./logos/mrbeast.svg" />
-    //   </div>
-
-    //   <div className="logos-slide">
-    //     <img src="./logos/3m.svg" />
-    //     <img src="./logos/barstool-store.svg" />
-    //     <img src="./logos/budweiser.svg" />
-    //     <img src="./logos/buzzfeed.svg" />
-    //     <img src="./logos/forbes.svg" />
-    //     <img src="./logos/macys.svg" />
-    //     <img src="./logos/menshealth.svg" />
-    //     <img src="./logos/mrbeast.svg" />
-    //   </div>
-    // </div>
     <div className="logos">
       <div className="logos-slide">
         <div>
@@ -65,6 +39,9 @@ const Carousel = () => {
         <div>
           <SiChakraui />
         </div>
+        <div>
+          <RiTailwindCssFill />
+        </div>
       </div>
       <div className="logos-slide">
         <div>
@@ -91,8 +68,11 @@ const Carousel = () => {
         <div>
           <SiChakraui />
         </div>
+        <div>
+          <RiTailwindCssFill />
+        </div>
       </div>
     </div>
-  );
-};
-export default Carousel;
+  )
+}
+export default Carousel
