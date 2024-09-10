80cbdd63f1c80163c829269fec4f330166336a36
ThembaMtshelwane <thembamm3@gmail.com>
2024-09-10 12:23:38 +0200

    removed old code

--- src/Components/HeroSection/Carousel.tsx
+++ src/Components/HeroSection/Carousel.tsx
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./carousel.css";
 
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
 import { FaHtml5 } from "react-icons/fa";
 import { IoLogoCss3 } from "react-icons/io5";
 import { IoLogoJavascript } from "react-icons/io5";
@@ -16,29 +12,6 @@ import { SiTypescript } from "react-icons/si";
 
 const Carousel = () => {
   return (
-    // <div className="logos">
-    //   <div className="logos-slide">
-    //     <img src="./logos/3m.svg" />
-    //     <img src="./logos/barstool-store.svg" />
-    //     <img src="./logos/budweiser.svg" />
-    //     <img src="./logos/buzzfeed.svg" />
-    //     <img src="./logos/forbes.svg" />
-    //     <img src="./logos/macys.svg" />
-    //     <img src="./logos/menshealth.svg" />
-    //     <img src="./logos/mrbeast.svg" />
-    //   </div>
-
-    //   <div className="logos-slide">
-    //     <img src="./logos/3m.svg" />
-    //     <img src="./logos/barstool-store.svg" />
-    //     <img src="./logos/budweiser.svg" />
-    //     <img src="./logos/buzzfeed.svg" />
-    //     <img src="./logos/forbes.svg" />
-    //     <img src="./logos/macys.svg" />
-    //     <img src="./logos/menshealth.svg" />
-    //     <img src="./logos/mrbeast.svg" />
-    //   </div>
-    // </div>
     <div className="logos">
       <div className="logos-slide">
         <div>
