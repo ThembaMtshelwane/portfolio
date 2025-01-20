4e92b9ab0975a0d34a07476c99e80d52eebe3df6
ThembaMtshelwane <thembamm3@gmail.com>
2025-01-20 11:15:37 +0200

    Added modal

---
+++ src/definitions.ts
@@ -0,0 +1,10 @@
+export type ProjectDataType = {
+  name: string;
+  techStack: string[];
+  summary: string;
+  responsibilities: string[];
+  website?: string;
+  githublink: string;
+  image: string;
+  status: string;
+};
